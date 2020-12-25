1a98d2beb4253509c594105b1c7178a41da20788
Jaime <jaimeloeuf@gmail.com>
2020-12-25 16:02:50 +0800

    Add firebase and its init code.

--- src/main.js
+++ src/main.js
@@ -4,14 +4,42 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-
 // Register global custom directive called `v-autofocus`
 import autofocus from "./directives/autofocus";
 Vue.directive("autofocus", autofocus);
+
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// firebaseConfig auto generated in project settings
+firebase.initializeApp({
+  apiKey: "AIzaSyAr5DhM52eu4Z1Ul2_9AzonR_zN2upmzJI",
+  authDomain: "dns-proxy-ui.firebaseapp.com",
+  projectId: "dns-proxy-ui",
+  appId: "1:701855010132:web:0acf68baf09c47ab9f0cc1",
+});
+
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+// App variable to store reference to the vue App object
+let app;
+
+/**
+ * @notice Why new vue is wrapped in this.
+ * Wait for firebase to finish initialization before creating the app.
+ * So that the router navigation wont break due to invalid auth
+ */
+const unsubscribe = firebase.auth().onAuthStateChanged(() => {
+  // Prevent app initialization from running more than once
+  if (!app)
+    // Create new vue app
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount("#app");
+
+  // Use the firebase.Unsubscribe function returned from adding auth state change listner to unsubscribe
+  // To prevent new Vue from running more than once
+  unsubscribe();
+});
