c2e1f39e0451df308d3154551acee649877399f6
Jaime <jaimeloeuf@gmail.com>
2020-12-25 13:45:48 +0800

    Copied in autofocus directive.

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+
+// Register global custom directive called `v-autofocus`
+import autofocus from "./directives/autofocus";
+Vue.directive("autofocus", autofocus);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
